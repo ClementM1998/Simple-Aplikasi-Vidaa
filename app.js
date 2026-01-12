d0603524e919bca4040a9d706c5d6229b95aab71
Clement Okie <83030323+ClementM1998@users.noreply.github.com>
2026-01-12 10:13:05 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+var button = document.getElementById("btn")
+var text = document.getElementById("text")
+
+button.addEventListener("click", function () {
+    text.innerText = "Button telah ditekan!"
+})
